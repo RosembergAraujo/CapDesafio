b905b3165d7962867a60361fe311e563377b4ad1
Rosemberg Araujo <70284263+RosembergAraujo@users.noreply.github.com>
2022-02-21 13:29:26 -0300

    Update Q1.java

--- src/Q1.java
+++ src/Q1.java
@@ -4,7 +4,7 @@ public class Q1 {
     public static void main(String[] args) {
         System.out.print("Entrada: ");
         int n = Integer.parseInt(new Scanner(System.in).next());
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             for (int j = n; j >= 0; j--) {
                 char s = j > i ? ' ' : '*';
                 System.out.print(s);
